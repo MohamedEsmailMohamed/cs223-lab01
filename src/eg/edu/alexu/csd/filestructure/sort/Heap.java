48363cb94433fdd4c61bafdda3399a3862102f69
EngHesham <heshammedhat5@gmail.com>
2018-02-23 00:07:59 +0200

    Heap skeleton

---
+++ src/eg/edu/alexu/csd/filestructure/sort/Heap.java
@@ -0,0 +1,47 @@
+package eg.edu.alexu.csd.filestructure.sort;
+
+/**
+ * Binary heap implementation.
+ * 
+ * @author H
+ *
+ */
+public class Heap<T extends Comparable<T>> implements IHeap<T> {
+
+	@Override
+	public INode<T> getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void heapify(INode<T> node) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T extract() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(T element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void build(java.util.Collection<T> unordered) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
